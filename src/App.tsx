e6edd66128b6622b04a797662b45ea0d7353e4b8
healthproava <kceleski@healthproassist.com>
2025-06-23 13:40:41 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -14,6 +13,22 @@ import FacilityMapPage from "./pages/FacilityMapPage";
 import AssessmentPage from "./pages/AssessmentPage";
 import RequireAuth from "./components/auth/RequireAuth";
 import AdminTools from "./pages/portal/AdminTools";
+import LoginPage from "./pages/LoginPage";
+import ClientsPage from "./pages/portal/ClientsPage";
+import CalendarPage from "./pages/portal/CalendarPage";
+import { 
+  PricingPage, 
+  AboutPage, 
+  BlogPage, 
+  KnowledgeBasePage, 
+  CaseStudiesPage, 
+  FAQPage, 
+  ApiDocsPage, 
+  ContactSalesPage, 
+  PrivacyPolicyPage, 
+  TermsOfServicePage, 
+  CookiePolicyPage 
+} from "./pages/PlaceholderPages";
 
 const queryClient = new QueryClient();
 
@@ -25,17 +40,62 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/assessment" element={<AssessmentPage />} />
           <Route path="/facilities-map" element={
             <RequireAuth>
               <FacilityMapPage />
             </RequireAuth>
           } />
-          <Route path="/portal/dashboard" element={<Dashboard />} />
-          <Route path="/portal/facilities" element={<Facilities />} />
-          <Route path="/portal/client/:id" element={<ClientDetails />} />
-          <Route path="/portal/ava" element={<AvaPortal />} />
-          <Route path="/portal/admin" element={<AdminTools />} />
+          <Route path="/portal/dashboard" element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          } />
+          <Route path="/portal/facilities" element={
+            <RequireAuth>
+              <Facilities />
+            </RequireAuth>
+          } />
+          <Route path="/portal/clients" element={
+            <RequireAuth>
+              <ClientsPage />
+            </RequireAuth>
+          } />
+          <Route path="/portal/client/:id" element={
+            <RequireAuth>
+              <ClientDetails />
+            </RequireAuth>
+          } />
+          <Route path="/portal/ava" element={
+            <RequireAuth>
+              <AvaPortal />
+            </RequireAuth>
+          } />
+          <Route path="/portal/admin" element={
+            <RequireAuth>
+              <AdminTools />
+            </RequireAuth>
+          } />
+          <Route path="/portal/calendar" element={
+            <RequireAuth>
+              <CalendarPage />
+            </RequireAuth>
+          } />
+          
+          {/* Placeholder Routes */}
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/knowledge-base" element={<KnowledgeBasePage />} />
+          <Route path="/case-studies" element={<CaseStudiesPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/api-docs" element={<ApiDocsPage />} />
+          <Route path="/contact-sales" element={<ContactSalesPage />} />
+          <Route path="/privacy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms" element={<TermsOfServicePage />} />
+          <Route path="/cookies" element={<CookiePolicyPage />} />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
